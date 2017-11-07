166434f1ab8ba31edc33bb74b14048e280cb6101
“Tea <teateearu@gmail.com>
2017-11-07 14:54:40 +0100

    Fixed defaults

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,16 +6,23 @@ class Event < ApplicationRecord
   validates :starts_at, presence: true
   validates :ends_at, presence: true
 
-after_initialize :init
-def init
-  self.price  ||= 0.0           #will set the default value only if it's nil
-  self.includes_food ||= false #let's you set a default association
-  self.includes_drinks ||= false
-  self.active ||= true
-end
-# after_initialize do
-#   default: 0
-# end
+  after_initialize :set_default_values
+    def set_default_values
+      self.price  ||= 0.0
+      self.includes_food ||= false
+      self.includes_drinks ||= false
+      self.active ||= true
+    end
+
   # inclusion: { in: (:starts_at+24.hours) }, after: :starts_at + 24.hours
+    validate :starts_at_before_ends_at
+
+    def starts_at_before_ends_at
+      if self.starts_at +24.hours <= self.ends_at
+        return true
+      else self.starts_at > self.ends_at
+        return false
+      end
+    end
 
 end
